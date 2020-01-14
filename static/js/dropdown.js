feb6d457cebe24f32c6251d616e75d67e3495ecf
David Williams <davidwilliams@Davids-MacBook-Pro-2.local>
2020-01-14 11:43:50 -0500

    add app3 for testng. Rearranged for flask

---
+++ static/js/dropdown.js
@@ -0,0 +1,12 @@
+let cat = cat;
+
+console.log(cat)
+
+// //Get the IDs
+// var samples = data.samples;
+// var optionValue = d3.select("select");
+// //Put the IDs into the dropdown list
+// samples.forEach((sample) => {
+//     optionValue.append("option")
+//     .text(sample.id)
+// });
